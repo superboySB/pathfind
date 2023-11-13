54527dd2ec087544b6c4ff12a2b1316390ecb444
Zipeng Dai <604896160@qq.com>
2023-11-13 22:19:47 +0800

    Update pathfinder.go

--- pathfinder.go
+++ pathfinder.go
@@ -155,7 +155,7 @@ func visibilityGraph(ps poly.PolygonSet, points []image.Point) graph[image.Point
 	var mutex sync.Mutex // 定义互斥锁
 
 	// 根据您的 CPU 逻辑核心数量来设置并发量
-	semaphore := make(chan struct{}, 64)
+	semaphore := make(chan struct{}, 48)
 
 	for i, a := range points {
 		wg.Add(1)
